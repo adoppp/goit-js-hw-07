a733af7d896e438595a93112c796433dd523bdeb
adoppp <121878757+adoppp@users.noreply.github.com>
2023-05-21 22:23:23 +0200

    вфз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,46 +4,46 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 
-const galleryMarkUp = document.querySelector('.gallery');
+const galleryList = document.querySelector('.gallery');
 
-const galleryEl = galleryItems
-    .map(({ preview, description, original }) => 
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-        </a>
-    </div>`)
-    .join('');
-
-galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl)
-
-galleryMarkUp.addEventListener('click', onImgClick)
-
-function onImgClick(evt) {
-    evt.preventDefault();
-
-    if (evt.target.nodeName !== 'IMG') {
-        return;
+// Создание и рендеринг разметки
+const createGalleryItem = ({ preview, original, description }) => {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = original;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = preview;
+    image.setAttribute('data-source', original);
+    image.alt = description;
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    return galleryItem;
+  };
+  const galleryMarkup = galleryItems.map(createGalleryItem);
+  galleryList.append(...galleryMarkup);
+  // Реализация делегирования и получение URL большого изображения
+  galleryList.addEventListener('click', handleGalleryItemClick);
+  function handleGalleryItemClick(event) {
+    event.preventDefault();
+    const target = event.target;
+    if (target.nodeName !== 'IMG') {
+      return;
     }
-
-    const modal = basicLightbox.create(
-        `<img src="${evt.target.dataset.source}" width="800" height="600">`,
-
-        {   onShow: () => window.addEventListener('keydown', onEscKeyPress),
-            onClose: () => window.removeEventListener('keydown', onEscKeyPress),
-        }
-    );
-    
-    modal.show();
-
-    function onEscKeyPress(evt) {   
-        if (evt.code === "Escape") {
-            modal.close();
-        }
-    }
-}
+    const source = target.dataset.source;
+    openModal(source);
+  }
+  // Открытие модального окна по клику на элементе галереи
+  function openModal(source) {
+    const instance = basicLightbox.create(`<img src="${source}" width="800" height="600">`);
+    instance.show();
+    // Закрытие модального окна при нажатии на кнопку Esc
+    const handleKeyPress = (event) => {
+      if (event.code === 'Escape') {
+        instance.close();
+      }
+    };
+    window.addEventListener('keydown', handleKeyPress);
+  }
